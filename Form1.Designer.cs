3dda57eeb01c0f6904522a0b4bb32387a3b89bb5
alice29shevchuk <Sevc_sd76@student.itstep.org>
2022-10-13 19:08:50 +0300

    test commit 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(177, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.Size = new System.Drawing.Size(139, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Hello from Alice";
+            this.label2.Text = "Hello from Alice jdsjd";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
