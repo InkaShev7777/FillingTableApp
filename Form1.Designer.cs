93947cb3e0605d82588e538fc1632847316467eb
alice29shevchuk <Sevc_sd76@student.itstep.org>
2022-10-13 19:45:24 +0300

    add btn close

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,17 +42,18 @@
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
             this.button1.Location = new System.Drawing.Point(24, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(181, 60);
+            this.button1.Size = new System.Drawing.Size(204, 60);
             this.button1.TabIndex = 0;
             this.button1.Text = "Download";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(224, 115);
+            this.textBox1.Location = new System.Drawing.Point(24, 110);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(509, 293);
+            this.textBox1.Size = new System.Drawing.Size(729, 293);
             this.textBox1.TabIndex = 1;
             // 
             // button2
@@ -61,28 +63,43 @@
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
             this.button2.Location = new System.Drawing.Point(24, 419);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(181, 53);
+            this.button2.Size = new System.Drawing.Size(204, 55);
             this.button2.TabIndex = 2;
             this.button2.Text = "Save result";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(426, 80);
+            this.label1.Location = new System.Drawing.Point(651, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(102, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "Result";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(582, 419);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
-            this.ClientSize = new System.Drawing.Size(762, 500);
+            this.ClientSize = new System.Drawing.Size(776, 500);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
@@ -100,6 +117,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,34 @@ namespace FillingTable
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = openFileDialog1.FileName;
+            // логика обработки и добавления в текст-бокс
+            string fileText = System.IO.File.ReadAllText(filename);
+            textBox1.Text = fileText;
+            MessageBox.Show("Файл открыт");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog1.FileName;
+            // сохраняем текст в файл
+            System.IO.File.WriteAllText(filename, textBox1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
